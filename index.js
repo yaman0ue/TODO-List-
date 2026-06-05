c4483e3acb7ab2f391b22323db2069680ac94fe0
yaman0ue <ymgmsouta@gmail.com>
2026-06-05 14:20:29 +0900

    課題

---
+++ index.js
@@ -0,0 +1,7 @@
+// 1. テキストボックスDOM要素の取得し、console.logで表示
+const inputElement = document.getElementById("add-input");
+console.log(inputElement);
+
+// 2. ボタンDOM要素を取得し、console.logで表示
+const addButton = document.getElementById("add-btn");
+console.log(addButton);
